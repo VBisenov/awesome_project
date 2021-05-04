1e03be480737c98210447b7e392a0e6904239053
vladimirbisenov <v.bisenov@mail.ru>
2021-05-04 10:02:16 +0400

    swagger init commit

--- src/main/java/com/example/awesome_project/controller/StudentController.java
+++ src/main/java/com/example/awesome_project/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.awesome_project.controller;
 
 import com.example.awesome_project.model.Student;
 import com.example.awesome_project.service.UtilService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,21 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+@Operation - Описывает операцию или обычно метод HTTP для определенного пути.
+@Parameter - Представляет один параметр в операции OpenAPI.
+@RequestBody - Представляет тело запроса в операции
+@ApiResponse - Представляет ответ в операции
+@Tag - Представляет теги для операции или определения OpenAPI.
+@Server - Представляет серверы для операции или для определения OpenAPI.
+@Callback - Описывает набор запросов
+@Link - Представляет возможную ссылку времени разработки для ответа.
+@Schema - Позволяет определять входные и выходные данные.
+@ArraySchema - Позволяет определять входные и выходные данные для типов массивов.
+@Content - Предоставляет схему и примеры для определенного типа мультимедиа.
+@Hidden - Скрывает ресурс, операцию или свойство
+ */
+
 @RestController
 @RequestMapping("/students")
 public class StudentController {
@@ -26,6 +42,7 @@ public class StudentController {
         this.utilService = utilService;
     }
 
+    @Operation(description = "Just test")
     @GetMapping(value = "/test")
     public String test() {
         return "HELLO WORLD";

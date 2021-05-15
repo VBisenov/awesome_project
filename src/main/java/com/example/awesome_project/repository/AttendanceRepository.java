74fec63c90b4e886c08d0efe23b984298215806f
vladimirbisenov <v.bisenov@mail.ru>
2021-05-15 15:40:40 +0400

    hello

--- src/main/java/com/example/awesome_project/repository/AttendanceRepository.java
+++ src/main/java/com/example/awesome_project/repository/AttendanceRepository.java
@@ -1,7 +1,7 @@
 package com.example.awesome_project.repository;
 
 import com.example.awesome_project.model.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; 
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 }
